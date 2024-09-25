1f8e1bc2b20d6ec1c2360a0e25ac737dfc51f8d1
Pierre <pierre6richard7@gmail.com>
2024-09-25 10:15:03 +0200

    feat: display habitant on the side

--- Scripts/WindowScript.gd
+++ Scripts/WindowScript.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
 var desc
+var display
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	desc = owner.find_child("Description")
+	display = owner.owner.find_child("Display").find_child("DisplayLabel")
 	pass # Replace with function body.
 
 
@@ -14,8 +16,7 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)):
-			print("window")
-			print(owner.habitant)
+			display.text = "Habitants:\n"+ str(owner.habitant)
 	elif event is InputEventMouseMotion:
 		if get_rect().has_point(to_local(event.position)):
 			desc.show()
