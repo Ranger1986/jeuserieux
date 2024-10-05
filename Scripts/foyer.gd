5856d0f47e86514f83fd542e7913e1fab5d6e966
Pierre <pierre6richard7@gmail.com>
2024-10-05 19:47:33 +0200

    feat: display bonheur global

--- Scripts/foyer.gd
+++ Scripts/foyer.gd
@@ -3,11 +3,11 @@ extends Button
 
 var etage
 var numero
-
+static var liste_foyer : Array
 var label : Label
 
 var habitant
-var happiness
+var bonheur
 var confiance
 var luminosite
 var son
@@ -19,12 +19,13 @@ static var foyer_cible
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	habitant = 1 + randi() % 2  # Random between 1 and 3
-	happiness = randi() % 101  # Random between 0 and 100
+	bonheur = randi() % 101  # Random between 0 and 100
 	confiance = randi() % 21   # Random between 0 and 20
 	luminosite = randi() % 101 # Random between 0 and 100
 	son = randi() % 101  # Random between 0 and 100
-	budget = 100 +randi() % 101
+	budget = 100 + randi() % 101
 	pressed.connect(self._button_pressed)
+	liste_foyer.append(self)
 
 func _button_pressed():
 	Foyer.set_cible(self)
@@ -33,7 +34,7 @@ func _button_pressed():
 func display_info():
 	label.text = "Appartement: " + name + "\n"
 	label.text += "Habitants:\n" + str(habitant) + "\n"
-	label.text += "Bonheur:\n" + str(happiness) + "%\n"
+	label.text += "Bonheur:\n" + str(bonheur) + "%\n"
 	label.text += "Confiance:\n" + str(confiance) + "%\n"
 	label.text += "Luminosité:\n" + str(luminosite) + "%\n"
 	label.text += "Sonorité:\n" + str(son) + "%\n"
@@ -48,3 +49,10 @@ static func set_cible(cible: Foyer) -> void:
 
 func add_money(amount: float) -> void:
 	budget += amount
+
+static func get_bonheur_moyen()->int:
+	var moyenne : int = 0
+	for foyer : Foyer in liste_foyer:
+		moyenne+=foyer.bonheur
+	moyenne/=liste_foyer.size()
+	return moyenne
--- Scripts/start.gd
+++ Scripts/start.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var budget_player: float
 var budget_player_label: Label
+var bonheur_label: Label
 
 func load_json(path: String):
 	if FileAccess.file_exists(path):
@@ -15,6 +16,8 @@ func load_json(path: String):
 		print("File doesn't exist!")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	budget_player_label = find_child("Budget_player")
+	bonheur_label = find_child("Bonheur")
 	budget_player = 10000.0
 	var store_item: Array = Item.load_items_from_json("res://items.json")
 	var scene = preload("res://Scene/item.tscn")
@@ -33,8 +36,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	budget_player_label = find_child("Budget_player")
-	budget_player_label.text = "Budget : " + str(budget_player)
+	budget_player_label.text = "Budget: " + str(budget_player)
+	bonheur_label.text = "Bonheur: " + str(Foyer.get_bonheur_moyen()) + "%"
 	pass
 
 # slot (s'appelle lors de la réception du signal)
